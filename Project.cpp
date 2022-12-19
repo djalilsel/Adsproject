1cbeb49257d5a031798ba026f3609b634aaa0737
djalil <selamnia.a.eldjalil@gmail.com>
2022-12-19 15:22:37 +0100

    added the names table

--- Project.cpp
+++ Project.cpp
@@ -1,12 +1,28 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 using namespace std;
 int main (){
-	int ;
-	double ;
-	char ;
-	string ;
-	struct table{
-		string N;
-	};talbe Na;
+	//int ;
+	//double ;
+	//char ;
+	//string ;
+//declaration pour le tableau des noms----------------
+	int i,n;
+//----------------------------------------------------
+	
+	
+//Tableau des noms----------------------------------------------------------------------
+	//id5aall
+	cout<<"Entrer la taille de tableau des noms: ";
+	cin>>n;
+	string noms[n]; //tableau des noms
+	for (i=0;i<n;i++){
+		cin>>noms[i];
+	}
+	//affichage------delete after finiched 'we don't need it in the project'---
+	cout<<"les utilisateur are:"<<endl;
+	for (i=0;i<n;i++){	
+	cout<<noms[i]<<endl;
+	}
+//-------------------------------------------------------------------------------------
 }
