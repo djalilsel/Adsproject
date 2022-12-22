71d7db40760f323722c0619520affff65ccad391
djalil <selamnia.a.eldjalil@gmail.com>
2022-12-22 12:46:22 +0100

    question 2

--- Project.cpp
+++ Project.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
 int main (){
 	//int ;
 	//double ;
 	//char ;
 	//string ;
-//declaration pour le tableau des noms----------------
-	int i,n;
-//----------------------------------------------------
 	
-	
+	//declaration pour le tableau des noms----------------
+	int i,n,j;
+	//declaration pour la question 2 x y sont amis----------------------------------------------------
+	int rt[i][j];
+	string N1,N2;
+	int a,b;
 //Tableau des noms----------------------------------------------------------------------
 	//id5aall
 	cout<<"Entrer la taille de tableau des noms: ";
@@ -21,8 +23,29 @@ int main (){
 	}
 	//affichage------delete after finiched 'we don't need it in the project'---
 	cout<<"les utilisateur are:"<<endl;
-	for (i=0;i<n;i++){	
+	for (i=0;i<n;i++){
 	cout<<noms[i]<<endl;
 	}
 //-------------------------------------------------------------------------------------
+//Question 2 x y sont des amis--------------------------------------------
+	cout<<"Entrer les noms:"<<endl<<"Nom 1: ";
+	cin>>N1;
+	cout<<"Nom 1: ";
+	cin>>N2;
+	
+	for(i=0;i<n;i++){
+		if(noms[i]==N1){
+			a=i;
+		}
+		if(noms[i]==N2){
+			b=i;
+		}		
+	}
+	if(rt[a][b]==1){
+		cout<<noms[a]<<" et "<<noms[b]<<" sont amis :)";
+	}
+	else{
+		cout<<noms[a]<<" et "<<noms[b]<<" ne sont pas amis :)";
+	}
+//------------------------------------------------------------------------
 }
